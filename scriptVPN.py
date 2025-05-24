e010ae69d669d8514ce32973a6af4a161bc73763
GeovanniVeloso <geoveloso543@gmail.com>
2025-05-24 19:36:07 -0300

    Script Finalizado

--- scriptVPN.py
+++ scriptVPN.py
@@ -4,30 +4,61 @@
 import sys
 import os
 import subprocess
+import zipfile
 
 nomeCliente = sys.argv[1]
 password = sys.argv[2]
+destino = os.path.join("/etc/openvpn/client/", nomeCliente)
+ipServidor = ""
+
+try:
+    result = subprocess.run(
+        ["ip", "-4", "addr", "show", "enp0s3"],
+        capture_output=True,
+        text=True,
+        check=True
+    )
+    for line in result.stdout.splitlines():
+        line = line.strip()
+        if line.startswith("inet "):
+            ipServidor =  line.split()[1].split("/")[0]  # Pega só o IP
+except subprocess.CalledProcessError:
+    print("Erro ao obter IP da interface enp0s3")
 
 try:
     subprocess.run(
-        ["/usr/share/easy-rsa/easyrsa", "gen-req", nomeCliente, "nopass"], check=True
+        ["./easyrsa","--batch", "gen-req", nomeCliente, "nopass"], cwd="/usr/share/easy-rsa/", check=True
     )
 except:
     print("Erro ao executar o gen-req")
 
+#try:
+#    subprocess.run(
+#        ["./easyrsa","--batch", "sign-req", "client", nomeCliente],
+#        input=f"\n{password}\n",
+#        text=True,
+#        check=True,
+#        cwd="/usr/share/easy-rsa/"
+#    )
+#except:
+#    print("Erro ao executar o sign-req")
+
+env = os.environ.copy()
+env["EASYRSA_PASSIN"] = f"pass:{password}"
+
 try:
     subprocess.run(
-        ["/usr/share/easy-rsa/easyrsa", "sign-req", "client", nomeCliente],
-        input=f"yes\n{password}",
-        text=True,
+        ["./easyrsa", "--batch", "sign-req", "client", nomeCliente],
         check=True,
+        cwd="/usr/share/easy-rsa/",
+        env=env
     )
-except:
+except subprocess.CalledProcessError:
     print("Erro ao executar o sign-req")
 
 try:
     subprocess.run(
-        ["mkdir", os.path.join("/etc/openvpn/clientes/", nomeCliente)], check=True
+        ["mkdir", os.path.join("/etc/openvpn/client/", nomeCliente)], check=True
     )
 except:
     print("Erro ao executar o mkdir")
@@ -37,7 +68,7 @@ try:
         [
             "cp",
             "/usr/share/easy-rsa/pki/ca.crt",
-            os.path.join("/etc/openvpn/cliente/", nomeCliente),
+            os.path.join("/etc/openvpn/client/", nomeCliente),
         ],
         check=True,
     )
@@ -48,8 +79,8 @@ try:
     subprocess.run(
         [
             "cp",
-            os.path.join("/usr/share/easy-rsa/pki/issued/", nomeCliente, ".crt"),
-            os.path.join("/etc/openvpn/clientes/", nomeCliente, "/"),
+            os.path.join("/usr/share/easy-rsa/pki/issued/", f"{nomeCliente}.crt"),
+            destino
         ],
         check=True,
     )
@@ -60,8 +91,8 @@ try:
     subprocess.run(
         [
             "cp",
-            os.path.join("/usr/share/easy-rsa/pki/private/", nomeCliente, ".key"),
-            os.path.join("/etc/openvpn/clientes/", nomeCliente, "/"),
+            os.path.join("/usr/share/easy-rsa/pki/private/", f"{nomeCliente}.key"),
+            destino,
         ],
         check=True,
     )
@@ -73,7 +104,7 @@ try:
         [
             "cp",
             os.path.join("/usr/share/easy-rsa/pki/dh.pem"),
-            os.path.join("/etc/openvpn/clientes/", nomeCliente, "/"),
+            destino,
         ],
         check=True,
     )
@@ -85,54 +116,47 @@ try:
         [
             "cp",
             "-r",
-            os.path.join("/etc/openvpn/clientes/", nomeCliente),
-            "/home/clientes/",
+            f"/etc/openvpn/client/{nomeCliente}",
+            "/home/usuario/",
         ],
         check=True,
     )
 except:
     print("Erro ao executar o cp -r")
 
-# Criando o certificado - cd /usr/share/easy-rsa/ | sudo ./easyrsa gen-req nomeCliente nopass
+ovpn_conteudo = f"""client
+dev tun
+proto udp
 
-# Criando o certificado P2 - cd /usr/share/easy-rsa/ | sudo ./easyrsa sign-req client nomeCliente
+remote {ipServidor}
 
-# Criando o certificado por cliente - sudo mkdir /etc/openvpn/client/nomeCliente -> OK
-# sudo cp /usr/share/easy-rsa/pki/ca.crt /etc/openvpn/client/nomeCliente -> OK
-# sudo cp /usr/share/easy-rsa/pki/issued/nomeCliente.crt /etc/openvpn/client/nomeCliente/ -> OK
-# sudo cp /usr/share/easy-rsa/pki/private/nomeCliente.key /etc/openvpn/client/nomeCliente/ -> Ok
-# sudo cp /usr/share/easy-rsa/pki/dh.pem /etc/openvpn/client/nomeCliente/ -> Ok
-# sudo cp -r /etc/openvpn/client/nomeCliente /home/clientes/
+ca ca.crt
+cert {nomeCliente}.crt
+key {nomeCliente}.key
 
-# Criando o arquivo ovpn - sudo nano /etc/openvpn/client/nomeCliente/nomeCliente.ovpn
+tls-client
+resolv-retry infinite
+nobind
+persist-key
+persist-tun
+"""
 
-# client
-# dev tun
-# proto udp
-
-# remote ip server 1194
-
-# ca ca.crt
-# cert nomeCliente.key
-# key nomeCliente.key
-
-# tls-client
-# resolv-retry infinite
-# nobind
-# persist-key
-# persist-tun
-
-# Movendo os arquivos para o cliente - cp /etc/openvpn/client/nomeCliente/nomeCliente.ovpn /home/clientes/nomeCliente/
-# Dando permissão - sudo chown -R usuario:usuario /home/clientes/nomeCliente
+try:
+    with open(os.path.join(
+                "/etc/openvpn/client/", nomeCliente,f"{nomeCliente}.ovpn"
+            ), "w") as f:
+        f.write(ovpn_conteudo)
+except Exception as e:
+    print("Erro ao criar o arquivo .ovpn")
 
 try:
     subprocess.run(
         [
             "cp",
             os.path.join(
-                "/etc/openvpn/client/", nomeCliente, "/", nomeCliente, ".ovpn"
+                "/etc/openvpn/client/", nomeCliente,f"{nomeCliente}.ovpn"
             ),
-            os.path.join("/etc/openvpn/clientes/", nomeCliente),
+            os.path.join("/home/usuario/", nomeCliente),
         ],
         check=True,
     )
@@ -141,7 +165,17 @@ except:
 
 try:
     subprocess.run(
-        ["chown", "-R", "usuario:usuario /home/clientes/", nomeCliente], check=True
+        ["chown", "-R", "usuario:usuario", f"/home/usuario/{nomeCliente}"], check=True
     )
 except:
     print("Erro ao executar o chown")
+
+destino = f"/home/usuario/{nomeCliente}.zip"
+origem = f"/home/usuario/{nomeCliente}"
+
+with zipfile.ZipFile(destino, 'w', zipfile.ZIP_DEFLATED) as zipf:
+        for root, dirs, files in os.walk(origem):
+            for file in files:
+                caminho_completo = os.path.join(root, file)
+                caminho_relativo = os.path.relpath(caminho_completo, origem)
+                zipf.write(caminho_completo, caminho_relativo)
